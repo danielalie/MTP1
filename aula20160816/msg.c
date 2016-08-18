ee01d54654b4ff745f0c142103fef6f304edcffb
danielalie <daniela_higawa@hotmail.com>
2016-08-18 11:02:41 -0300

    consertando problema

--- aula20160816/msg.c
+++ aula20160816/msg.c
@@ -1,1 +1,15 @@
-/* NADA */
+#include <stdio.h>
+#include <stdlib.h>
+int main (int argc, char *argv[]){
+	int i;
+	if (argc == 1)
+		printf("bom dia!\n");
+	else {
+		if (argc == 2)
+			printf ("bom dia, %s!\n", argv[1]);
+		else
+			for (i=0; i<atoi(argv[1]); i++)
+				printf("bom dia, %s!\n", argv[2]);
+	}
+	return 0;
+}
