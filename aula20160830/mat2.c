9553fcdeec053b089821a6c42ee719ae6f914874
danielalie <daniela_higawa@hotmail.com>
2016-09-06 10:12:38 -0300

    Update mat2.c

--- aula20160830/mat2.c
+++ aula20160830/mat2.c
@@ -2,15 +2,14 @@
 #include<math.h>
 int main ()
 {
-    float lado1, lado2, lado3, angulorad;
-    float cos (float angulorad);
+    double lado1, lado2, lado3, angulorad;
     printf("informe o lado 1 do triangulo: ");
-    scanf ("%f", &lado1);
+    scanf ("%lf", &lado1);
     printf ("informe o lado 2 do triangulo: ");
-    scanf ("%f", &lado2);
+    scanf ("%lf", &lado2);
     printf ("informe o angulo entre os lados 1 e 2, em radianos: ");
-    scanf ("%f", &angulorad);
-    lado3 = sqrt ((lado1*lado1)+(lado2*lado2)-(2*lado1*lado2*cos(angulorad)));
+    scanf ("%lf", &angulorad);
+    lado3 = sqrt (pow(lado1,2)+pow(lado2,2)-2*lado1*lado2*cos(angulorad));
     printf("o lado que falta e = %f", lado3);
     return 0;
 }
