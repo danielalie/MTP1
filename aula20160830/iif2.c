8357b1f9c1d4ffef672624183769cc10f4b93448
danielalie <daniela_higawa@hotmail.com>
2016-09-06 09:54:04 -0300

    Update iif2.c

--- aula20160830/iif2.c
+++ aula20160830/iif2.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
+#include<stdint.h>
+int main ()
+{
+    double fatorial=1;
+    int i;
+    int numero;
+    printf ("fatorial de: ");
+    scanf ("%d", &numero );
+    for (i=2; i<=numero; i++)
+        fatorial=fatorial*i;
+    printf("o fatorial de %d  e %lf\n", numero, fatorial);
+    return 0;
+
+}
 
