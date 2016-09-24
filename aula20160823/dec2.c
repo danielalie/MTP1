f78ecdb697987361dba74d9d1687fdb26db7bd33
danielalie <daniela_higawa@hotmail.com>
2016-09-24 17:35:11 -0300

    Update dec2.c

--- aula20160823/dec2.c
+++ aula20160823/dec2.c
@@ -2,18 +2,19 @@
 #include<time.h>
 int main ()
 {
-    srand (time (0));
-    int numsorteado, numdigitado, resultado;
-    numsorteado = rand ()%1 + 1;
-    printf ("numero sorteado: %d\n", numsorteado);
+
+    int numdigitado, resultado;
     printf("digite um numero inteiro nao negativo: ");
-    scanf ("%f", &numdigitado);
-    resultado = numdigitado+numsorteado;
-    printf("Resultado: %d\n", resultado);
+    scanf ("%d", &numdigitado);
+    int aleat;
+    srand (time (0));
+    aleat = rand ()%2;
+    printf ("numero sorteado: %d\n",aleat);
+    resultado = numdigitado+aleat;
     if (resultado%2 == 0)
-        printf("o numero e par!\n");
+        printf("o resultado e: %d e o numero e par!\n",resultado);
     else
-        printf("o numero e impar!\n");
+        printf("o resultado e: %d e o numero e impar!\n", resultado);
     return 0;
 
 }
