b2a21e2497d680da52797f8222e479f85558af52
danielalie <daniela_higawa@hotmail.com>
2016-09-26 14:36:04 -0300

    fim

--- aula20160906/ptr3.c
+++ aula20160906/ptr3.c
@@ -1,17 +1,14 @@
-#include<stdio.h>
-int main ()
-{
-    int vetor[] = {0xFF,0xFFFF,0xFFFFFF,0xFFFFFFFF}
-    int tam = sizeof (vetor)/sizeof (int);//sizeof mostra tamanho vetor em bits; 10/4=5 numero elementos
-    int contagem = 0;
+#include <stdio.h>
+int main () {
+     int contagem = 0;
+     int vetor[] = {0xFF,0xFFFF,0xFFFFFF,0xFFFFFFFF};
+     int tam = sizeof(vetor)/sizeof(int);//sizeof mostra tamanho vetor em bits; 10/4=5 numero elementos
     unsigned char * ponteiro = NULL;
     ponteiro = (unsigned char *) vetor;//primeiro vetor eh o proprio vetor (vetor=vetor[0])
-    for (;ponteiro < vetor + tam ; ponteiro++)
-    {
-        printf ("%p: %X\n", ponteiro, *ponteiro);//mostra o ponteiro e seu conteudo
-        if (*ponteiro != 0x0)
-            contagem ++;
+    for(; ponteiro < vetor + tam ; ponteiro++){
+        printf("%p : %X\n", ponteiro, *ponteiro);//mostra o ponteiro e seu conteudo
+        if(*ponteiro == 0XFF) contagem++;
     }
-    printf ("sao %d os bytes de memoria com apenas 1's.\n", contagem);
+    printf("Sao %d os bytes de memoria com apenas 1's.\n", contagem);
     return 0;
 }
--- aula20160906/vet3.c
+++ aula20160906/vet3.c
@@ -1,16 +1,15 @@
-#include<stdio.h>
-int main ()
-{
+#include <stdio.h>
+int main () {
     int vet[10];
     int i, soma=0, produto=1;
-    for(i=0;i<10<i++)
-    {
-        printf("Digite o %d numero do vetor", i+1);
-        scanf ("%d",&vet[i]);
+    for(i=0; i<10; i++){
+        printf("Digite o %d numero do vetor: ",i+1);
+        scanf("%d",&vet[i]);
         soma=soma+vet[i];
         produto=produto*vet[i];
     }
-    printf ("soma: %d", soma);
-    printf("\n produto: %d", produto);
+    printf("A soma e: %d",soma);
+    printf("\nO produto e: %d",produto);
     return 0;
 }
+
