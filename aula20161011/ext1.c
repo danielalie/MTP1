0898e33c4ef888f0a88e3ec4babbcc165ae98000
danielalie <daniela_higawa@hotmail.com>
2016-11-08 23:20:11 -0200

    fim

--- aula20161011/ext1.c
+++ aula20161011/ext1.c
@@ -1,29 +1,70 @@
-
 #include <stdio.h>
 #include <stdlib.h>
-float *recebenumero (float *vetor, int *tamanho);
+float *recebeNumero (float *vetor,int *tamanho);
+float media( float *vetor,int *tamanho );
+float Maior(float *vetor,int *tamanho);
+float Menor(float*vetor,int *tamanho);
+
 int main ()
 {
-    float* vetor=NULL;
-    int tamanho=0, op;
-    do
+    int n;
+    float * vetor;
+    int tamanho=0;
+    vetor=(float *)malloc(sizeof(float));
+    do{
+    printf("\nCaso deseje sair digite 0,para entrar no programa digite qualquer outro numero: ");
+    scanf("%d",&n);
+    if(n!=0)
     {
-        vetor= recebenumero(vetor, &tamanho);
-        printf("Deseja entrar com um numero? (0-nao; 1-sim.");
+    vetor=recebeNumero(vetor,&tamanho);
+    printf("\nA media dos numeros eh: %f",media(vetor,&tamanho));
+    printf("\nO maior dos numeros eh: %f",Maior(vetor,&tamanho));
+    printf("\nO menor dos numeros eh: %f",Menor(vetor,&tamanho));
     }
-    while (op !=0);
+    }while(n!=0);
     free(vetor);
     return 0;
 }
 
-float *recebenumero (float *vetor, int *ptamanho)
+float *recebeNumero (float *vetor,int *tamanho)
 {
-    (*ptamanho)++;
-    if(vetor==NULL)
-        vetor= (float*) malloc(sizeof(float));
-    else
-        vetor= (float*) realloc(vetor, (*ptamanho)*sizeof(float));
-    printf("Entre com um numero: ");
-    scanf("%f", &vetor[(*ptamanho)-1]);
+    float numero1, numero2;
+    printf("Digite um numero: ");
+    scanf("%f",&numero1);
+    (*tamanho)++;
+    vetor =(float*) realloc(vetor,(*tamanho+1)*sizeof(float));
+    vetor[*tamanho-1]=numero;
     return vetor;
 }
+
+float media( float *vetor,int *tamanho )
+{
+    int i;
+    float soma=0;
+    for (i=0;i<*tamanho;i++)
+    {
+        soma=soma+vetor[i];
+    }
+    return soma/ *tamanho;
+}
+
+float Maior(float *vetor,int *tamanho)
+{
+    int i;
+    float maior=vetor[0];
+    for (i=0;i<*tamanho;i++)
+       if (maior<vetor[i])
+        maior=vetor[i];
+   return maior;
+}
+
+float Menor(float*vetor,int *tamanho)
+{
+    int i;
+    float menor=vetor[0];
+    for (i=0;i<*tamanho;i++)
+       if (menor>vetor[i])
+        menor=vetor[i];
+    return menor;
+}
+
