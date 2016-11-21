1dc57b9cad7ca5629f1865d26c1316c9b0253ca6
danielalie <daniela_higawa@hotmail.com>
2016-11-21 19:29:58 -0200

    fim

--- aula20161018/lei2.c
+++ aula20161018/lei2.c
@@ -1,23 +1,20 @@
-#include <stdio.h>
-char * leTexto (char * nome);
+#include<stdio.h>
 int main()
 {
+    FILE*arquivo;
+    char *dado[256];
     char * nome[256];
-    printf ("Informe o nome do arquivo para ser lido: ");
-    fgets (nome,256,stdin);
-    leTexto(nome);
+    printf("Qual arquivo deseja abrir? ");
+    scanf("%s",nome);
+    arquivo=fopen(nome,"r");
+    if(arquivo==NULL)
+        printf("\nArquivo nao encontrado\n");
+    else
+    {
+         fgets(dado,256,arquivo);
+        fscanf(arquivo,"%s",dado);
+        fclose(arquivo);
+        fprintf(stdout,"%s",dado);
+    }
     return 0;
 }
-
-char * leTexto (char *nome){
-    FILE *arquivo;
-    char *aux;
-    char * texto;
-    int c, tamanho = 0;
-    arquivo = fopen(nome, "r");
-    if(arquivo == NULL)
-       fscanf (nome,%f)
-    fclose(arquivo);
-    return texto;
-
-    }
