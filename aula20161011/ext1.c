864a3f2ab2138f638069c11817c0bc65bc165b05
danielalie <daniela_higawa@hotmail.com>
2016-11-21 19:17:07 -0200

    fim

--- aula20161011/ext1.c
+++ aula20161011/ext1.c
@@ -1,70 +1,85 @@
-#include <stdio.h>
-#include <stdlib.h>
-float *recebeNumero (float *vetor,int *tamanho);
-float media( float *vetor,int *tamanho );
-float Maior(float *vetor,int *tamanho);
-float Menor(float*vetor,int *tamanho);
+#include<stdio.h>
+#include<stdlib.h>
+int recebenumeros(int *numeros,int n);
+void maioremenor(int *numeros,int n);
+void media(int *numeros,int n);
+int main()
 
-int main ()
 {
-    int n;
-    float * vetor;
-    int tamanho=0;
-    vetor=(float *)malloc(sizeof(float));
-    do{
-    printf("\nCaso deseje sair digite 0,para entrar no programa digite qualquer outro numero: ");
-    scanf("%d",&n);
-    if(n!=0)
-    {
-    vetor=recebeNumero(vetor,&tamanho);
-    printf("\nA media dos numeros eh: %f",media(vetor,&tamanho));
-    printf("\nO maior dos numeros eh: %f",Maior(vetor,&tamanho));
-    printf("\nO menor dos numeros eh: %f",Menor(vetor,&tamanho));
-    }
-    }while(n!=0);
-    free(vetor);
+    int *numeros=malloc(sizeof(int));
+    int n,op,k;
+     printf("Quantos numeros deseja? ");
+        scanf("%d",&n);
+       numeros= recebenumeros(numeros,n);
+   do
+   {
+
+
+        printf("Escolha:\n(1)Media\n(2)Maior e Menor numero\n(3)Mais numeros?\n(4)Sair\n");
+        scanf("%d",&op);
+        switch(op)
+        {
+            case 1:
+                media(numeros,n);
+            break;
+            case 2:
+                maioremenor(numeros,n);
+            break;
+            case 3:
+                printf("Quantos a mais? ");
+                scanf("%d",&k);
+                n=n+k;
+                numeros=recebenumeros(numeros,n);
+                break;
+        }
+
+    }while(op!=4);
+    free(numeros);
     return 0;
 }
-
-float *recebeNumero (float *vetor,int *tamanho)
+int recebenumeros(int *numeros,int n)
 {
-    float numero1, numero2;
-    printf("Digite um numero: ");
-    scanf("%f",&numero1);
-    (*tamanho)++;
-    vetor =(float*) realloc(vetor,(*tamanho+1)*sizeof(float));
-    vetor[*tamanho-1]=numero;
-    return vetor;
-}
-
-float media( float *vetor,int *tamanho )
-{
-    int i;
-    float soma=0;
-    for (i=0;i<*tamanho;i++)
+    int i=0;
+    printf("Digite os numeros: \n");
+    while(i<n)
     {
-        soma=soma+vetor[i];
+        scanf("%d",&numeros[i]);
+        numeros=(int *)realloc(numeros,(n+1)*sizeof(int));
+        i++;
+
     }
-    return soma/ *tamanho;
+    system("cls");
+    return numeros;
 }
-
-float Maior(float *vetor,int *tamanho)
+void media(int *numeros,int n)
 {
-    int i;
-    float maior=vetor[0];
-    for (i=0;i<*tamanho;i++)
-       if (maior<vetor[i])
-        maior=vetor[i];
-   return maior;
+   int aux=0,i;
+   for(i=0;i<n;i++)
+   {
+       aux=aux+numeros[i];
+   }
+    system("cls");
+    printf("Media: %g\n\n",(float)aux/n);
+
+
 }
-
-float Menor(float*vetor,int *tamanho)
+void maioremenor(int *numeros,int n)
 {
-    int i;
-    float menor=vetor[0];
-    for (i=0;i<*tamanho;i++)
-       if (menor>vetor[i])
-        menor=vetor[i];
-    return menor;
+    system("cls");
+    int i,p=numeros[0];
+    for(i=0;i<n;i++)
+    {
+        if(numeros[i]>p)
+            p=numeros[i];
+    }
+    printf("Maior: %d\n",p);
+    p=numeros[0];
+    for(i=0;i<n;i++)
+    {
+        if(numeros[i]<p)
+            p=numeros[i];
+    }
+    printf("Menor: %d\n",p);
+
 }
 
